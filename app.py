b2eb08ee595e9499056cc3e9fff5e7f01c3ec2f8
Uladzimir Boki <uladzimir.boki@innowise.com>
2025-08-12 10:43:01 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+# app.py
+from flask import Flask
+
+# Инициализация приложения Flask
+app = Flask(__name__)
+
+# Определение маршрута для корневого URL
+@app.route('/')
+def hello_aws():
+    """
+    Returns a simple greeting message.
+    """
+    return 'Hello, AWS!'
+
+# Запуск приложения на порту 8080, если скрипт запущен напрямую
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
